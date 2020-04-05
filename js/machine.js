307b240334daf3e6f544d1596fa77b3a2244f021
adamtheiner <60622499+adamtheiner@users.noreply.github.com>
2020-04-05 21:35:32 +0300

    Update machine.js

--- js/machine.js
+++ js/machine.js
@@ -3,21 +3,27 @@ var machineGR = document.getElementById('mmachineGR');
 var drawingSVG = document.getElementById('drawing');
 var svgFrame = document.getElementsByTagName('svg')[0];
 var sizes = ComputeSizes();
-console.log(svgFrame)
 
 function ComputeSizes () { //вычисление размеров видимой области окна браузера
 	let windowWidth = document.body.clientWidth;
 	let windowHeight = document.body.clientHeight;
 	let headerHeight = document.getElementById('header').clientHeight;
 	let grHeight = windowHeight - headerHeight - 3;
-	let machineZoneX = svgFrame.clientWidth / 2;
-	let s = {Width:windowWidth, Height:windowHeight, HeaderHeight:headerHeight, GraphicHeight:grHeight, machineZoneX};
+	let machineWidth = svgFrame.clientWidth / 2 + svgFrame.clientWidth / 4;
+	let s = {Width:windowWidth, Height:windowHeight, HeaderHeight:headerHeight, GraphicHeight:grHeight};
 	
 	return s;
 }
 
 machineGR.setAttribute('height', sizes.GraphicHeight);
-
 svgFrame.setAttribute('height', sizes.GraphicHeight - sizes.GraphicHeight/4);
 
 console.log(sizes);
+
+DrawMachine();
+
+//================================
+
+function DrawMachine () {
+	DrawRoundedRect (10, 10, 290, 380, 4, 2, '#369', '#fff');
+}
