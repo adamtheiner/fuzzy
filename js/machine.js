b36a3a65170d1386165c0a52bcb1d558d31d3ba4
adamtheiner <60622499+adamtheiner@users.noreply.github.com>
2020-04-05 18:17:04 +0300

    Update machine.js

--- js/machine.js
+++ js/machine.js
@@ -1,12 +1,21 @@
 'use strict';
 var sizes = ComputeSizes();
+var machineGR = document.getElementById('mmachineGR');
+var drawingSVG = document.getElementById('drawing');
+var svgFrame = document.getElementsByTagName('svg')[0];
 
-function ComputeSizes () {
+function ComputeSizes () { //вычисление размеров видимой области окна браузера
 	let windowWidth = document.body.clientWidth;
 	let windowHeight = document.body.clientHeight;
-	let s = {Width:windowWidth, Height:windowHeight};
+	let headerHeight = document.getElementById('header').clientHeight;
+	let GrHeight = windowHeight - headerHeight - 3;
+	let s = {Width:windowWidth, Height:windowHeight, HeaderHeight:headerHeight, GraphicHeight:GrHeight};
 	
 	return s;
 }
 
-console.log(sizes.Width);
+machineGR.setAttribute('height', sizes.GraphicHeight);
+
+svgFrame.setAttribute('height', sizes.GraphicHeight - sizes.GraphicHeight/4);
+
+console.log(sizes);
